3f7be8a1ff1a892202fd6e675bd55c12d64aac17
asohee48 <sarapark0602@gmail.com>
2026-07-20 21:47:57 +0900

    [CA-16] fix: 소셜 로그인 중 발생하는 모든 예외 로그인 실패로 처리

--- src/main/java/com/swyp/BE/domain/auth/service/CustomOAuth2UserService.java
+++ src/main/java/com/swyp/BE/domain/auth/service/CustomOAuth2UserService.java
@@ -5,13 +5,16 @@ import com.swyp.BE.domain.user.dto.CustomOAuth2User;
 import com.swyp.BE.domain.user.entity.User;
 import com.swyp.BE.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -23,13 +26,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
-        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-        OAuthAttributes attributes = OAuthAttributes.of(registrationId, oAuth2User.getAttributes());
+        try {
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            OAuthAttributes attributes = OAuthAttributes.of(registrationId, oAuth2User.getAttributes());
 
-        User user = userRepository.findByProviderAndProviderId(attributes.getProvider(), attributes.getProviderId())
-                .map(existing -> existing.updateNickname(attributes.getNickname()))
-                .orElseGet(() -> userRepository.save(attributes.toEntity()));
+            User user = userRepository.findByProviderAndProviderId(attributes.getProvider(), attributes.getProviderId())
+                    .map(existing -> existing.updateNickname(attributes.getNickname()))
+                    .orElseGet(() -> userRepository.save(attributes.toEntity()));
 
-        return new CustomOAuth2User(user.getId(), user.getRole(), oAuth2User.getAttributes());
+            return new CustomOAuth2User(user.getId(), user.getRole(), oAuth2User.getAttributes());
+        } catch (Exception e) {
+            log.error("OAuth2 로그인 처리 중 오류 발생", e);
+            throw new OAuth2AuthenticationException(new OAuth2Error("login_processing_error"), e);
+        }
     }
 }
